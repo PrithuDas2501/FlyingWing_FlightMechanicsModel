3d090384c35f4b519aa633840f625ee2688ba6ae
Hrishav Das <72787333+PrithuDas2501@users.noreply.github.com>
2024-02-22 22:08:10 +0530

    Update paper_plots.m

--- Linearized_Matrices/paper_plots.m
+++ Linearized_Matrices/paper_plots.m
@@ -1,5 +1,4 @@
-clc; clear; 
-
+clc; clear;
 h2 = load("h2_design.mat");
 hinf = load('hinf_design.mat');
 %%
@@ -7,141 +6,90 @@ d2r = pi/180;
 A = h2.Aclp;
 B = [h2.Bd h2.Bu];
 B = [B; zeros(3,4)];
-Scale = [1/(0.5*d2r) 0; 0 1/10];
-%Scale = eye(2);
+%Scale = [1/(5*d2r) 0; 0 1/10]; %%%%% DOUBT REGARDING THIS! -- This is correct. We want to plot normalized z.
+Scale = eye(2);
 Cz = Scale*[1 0 -1 0; 0 1 0 0];
 C = [Cz zeros(2,3);zeros(3,4) eye(3)];
-sys = ss(A,B,C,[]);
-T = 0:0.01:1000;
-N = length(T);
-dalpha = (rand(N,1)-0.5)*2;
-dalpha_bar = dalpha/(trapz(T,dalpha.^2))^0.5;
-Wa = diag(h2.kappa.^-0.5);
-wa = (rand(N,3)-0.5)*2;
-wa_bar = wa/(trapz(T,(wa.^2)*[1;1;1])^0.5);
-u = [dalpha_bar wa_bar*Wa]; 
-X0 = [0;0;0;0;0;0;0];
-sol_h2 = lsim(sys,u,T,X0);
+sys_h2 = ss(A,B,C,[]);
 
+Tend = 1000;
+T = 0:0.1:Tend;
+N = length(T);
+dalpha = (rand(N,1)-0.5)*2 + sin(0.05*T)' + sin(0.01*T)' + sin(1*T)';
+dalpha_bar = dalpha/(trapz(T,dalpha.^2))^0.5;
+Wd = 10;
+
+Wa = diag(h2.kappa.^-0.5);
+%Wa = 0*eye(3);
+wa = (rand(N,3)-0.5)*2;
+
+
+wa_bar = wa/(trapz(T,(wa.^2)*[1;1;1])^0.5);
+u = [Wd*dalpha_bar wa_bar*Wa]; %%%%%% Should 0.01 be there for dalpha? Yes
+X0 = [0;0;0;0;0;0;0];
+sol_h2 = lsim(sys_h2,u,T,X0);
 
 A = hinf.Aclp;
-B = [hinf.Bd h2.Bu];
+B = [hinf.Bd hinf.Bu];
 B = [B; zeros(3,4)];
 Cz = Scale*[1 0 -1 0; 0 1 0 0];
 C = [Cz zeros(2,3);zeros(3,4) eye(3)];
-sys = ss(A,B,C,[]);
-T = 0:0.01:1000;
-N = length(T);
-dalpha = (rand(N,1)-0.5)*2;
-dalpha_bar = dalpha/(trapz(T,dalpha.^2))^0.5;
+sys_hinf = ss(A,B,C,[]);
+
+% T = 0:0.1:Tend;
+% N = length(T);
+% dalpha = (rand(N,1)-0.5)*2 + 10*sin(0.1*T)';
+% dalpha_bar = dalpha/(trapz(T,dalpha.^2))^0.5;
 Wa = diag(hinf.kappa.^-0.5);
-wa = rand(N,3);
+%Wa = 0*eye(3);
+%wa = (rand(N,3)-0.5)*2;
 wa_bar = wa/(trapz(T,(wa.^2)*[1;1;1])^0.5);
-u = [dalpha_bar wa_bar*Wa]; 
+u = [Wd*dalpha_bar wa_bar*Wa]; %%%%%% Shohuld 0.01 be there for dalpha? Yes
 X0 = [0;0;0;0;0;0;0];
-sol_hinf = lsim(sys,u,T,X0);
-%% H2 Plots
+sol_hinf = lsim(sys_hinf,u,T,X0);
+
+sys_open = ss(h2.A,h2.Bd,Cz,[]);
+X0 = [0;0;0;0];
+u = Wd*dalpha_bar;
+sol_open = lsim(sys_open,u,T,X0);
+
+%  H2 Plots
 limit = ones(N,1);
-figure;
+figure(4); clf;
 % Subplot 1
 subplot(2, 1, 1);hold on; grid on
 plot(T,sol_h2(:,1),'b')
-plot(T,limit,LineWidth=2,Color='r')
-plot(T,-limit,LineWidth=2,Color='r')
-title('Flight Path Angle Variation');
-ylim([-1.2 1.2])
+plot(T,sol_open(:,1),'r')
+% plot(T,limit,LineWidth=2,Color='r')
+% plot(T,-limit,LineWidth=2,Color='r')
+title('Flight Path Angle Variation (H2)');
+% ylim([-1.2 1.2])
 
 % Subplot 2
 subplot(2, 1, 2);hold on; grid on
 plot(T,sol_h2(:,2),'b')
-plot(T,limit,LineWidth=2,Color='r')
-plot(T,-limit,LineWidth=2,Color='r')
+plot(T,sol_open(:,2),'r')
+% plot(T,limit,LineWidth=2,Color='r')
+% plot(T,-limit,LineWidth=2,Color='r')
 title('Flight Velocity Variation');
-ylim([-1.2 1.2])
-%% Hinf Plots
-figure;
+% ylim([-1.2 1.2])
+
+%  Hinf Plots
+figure(5); clf;
 % Subplot 1
 subplot(2, 1, 1);hold on; grid on
 plot(T,sol_hinf(:,1),'b')
-plot(T,limit,LineWidth=2,Color='r')
-plot(T,-limit,LineWidth=2,Color='r')
-title('Flight Path Angle Variation');
-ylim([-1.2 1.2])
+plot(T,sol_open(:,1),'r')
+% plot(T,limit,LineWidth=2,Color='r')
+% plot(T,-limit,LineWidth=2,Color='r')
+title('Flight Path Angle Variation (Hinf)');
+% ylim([-1.2 1.2])
 
 % Subplot 2
 subplot(2, 1, 2);hold on; grid on
 plot(T,sol_hinf(:,2),'b')
-plot(T,limit,LineWidth=2,Color='r')
-plot(T,-limit,LineWidth=2,Color='r')
+plot(T,sol_open(:,2),'r')
+% plot(T,limit,LineWidth=2,Color='r')
+% plot(T,-limit,LineWidth=2,Color='r')
 title('Flight Velocity Variation');
-ylim([-1.2 1.2])
-%%
-fs = 14;
-
-% Plot control rate
-figure(1); clf;
-labels = {'$T$','$\delta_e$','$\delta_{lef}$'};
-stem(h2.omc,'r','filled','MarkerSize',8,'LineWidth', 1); hold on;
-stem(hinf.omc,'k-.','filled','MarkerSize',8,'LineWidth', 2);
-
-set(gca,'yscal','log');
-xaxisproperties= get(gca, 'XAxis');
-xaxisproperties.TickLabelInterpreter = 'latex';
-a = get(gca,'XTickLabel'); set(gca,'XTickLabel',a,'fontsize',12)
-
-title('Minimum Actuator Cut-off Frequencies (rad/s).','Interpreter','latex','FontSize',fs);
-xticks([1,2,3])
-xticklabels(labels);
-xlabel('Actuator');
-
-legend({'$\mathcal{H}_2$','$\mathcal{H}_\infty$'},'Interpreter','latex')
-grid on;
-
-print -depsc ../images/act_cutoff.eps
-
-% Plot DC gain
-figure(2); clf;
-labels = {'$T$','$\delta_e$','$\delta_{lef}$'};
-
-for i = 1:3
-    h2.hinf_norms(i) = norm(h2.act(i,:),inf);
-    hinf.hinf_norms(i) = norm(hinf.act(i,:),inf);
-end
-
-stem(h2.hinf_norms,'r','filled','MarkerSize',8,'LineWidth', 1); hold on;
-stem(hinf.hinf_norms,'k-.','filled','MarkerSize',8,'LineWidth', 2);
-
-set(gca,'yscal','log');
-xaxisproperties= get(gca, 'XAxis');
-xaxisproperties.TickLabelInterpreter = 'latex';
-
-title('Actuator $\mathcal{H}_\infty$ norms.','Interpreter','latex','FontSize',fs);
-xticks([1,2,3])
-xticklabels(labels);
-xlabel('Actuator');
-a = get(gca,'XTickLabel'); set(gca,'XTickLabel',a,'fontsize',12)
-
-legend({'$\mathcal{H}_2$','$\mathcal{H}_\infty$'},'Interpreter','latex')
-grid on;
-print -depsc ../images/act_hinfnorms.eps
-
-% Actuator noise
-figure(3); clf;
-labels = {'$T$','$\delta_e$','$\delta_{lef}$'};
-
-stem(1./sqrt(h2.kappa),'r','filled','MarkerSize',8,'LineWidth', 1); hold on;
-stem(1./sqrt(hinf.kappa),'k-.','filled','MarkerSize',8,'LineWidth', 2);
-
-set(gca,'yscal','log');
-xaxisproperties= get(gca, 'XAxis');
-xaxisproperties.TickLabelInterpreter = 'latex';
-
-title('Actuator Noise Scaling ($1/\sqrt{\kappa}$).','Interpreter','latex','FontSize',fs);
-xticks([1,2,3])
-xticklabels(labels);
-xlabel('Actuator');
-a = get(gca,'XTickLabel'); set(gca,'XTickLabel',a,'fontsize',12)
-
-legend({'$\mathcal{H}_2$','$\mathcal{H}_\infty$'},'Interpreter','latex')
-grid on;
-print -depsc ../images/act_noise.eps
+% ylim([-1.2 1.2])
